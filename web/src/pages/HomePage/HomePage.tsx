89f67b7172f87989e203da32b9df48ab6900be6a
xingxiang <xingxiang@twotreesgroup.com>
2022-10-11 11:58:47 +0800

    removed old packages

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import { MetaTags } from '@redwoodjs/web'
 import ItemsCell from 'src/components/Item/ItemsCell'
 
 const HomePage = () => {
+  // TODO: Add a button to "Export all to CSV"
   return (
     <>
       <MetaTags title="Home" description="Home page" />
