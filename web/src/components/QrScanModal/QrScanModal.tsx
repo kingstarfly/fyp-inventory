bbd1bf27785f791ff3c422a32e31b6862dad1abf
xingxiang <xingxiang@twotreesgroup.com>
2023-02-19 11:14:16 +0800

    removed redundant qr code checkbox

--- web/src/components/QrScanModal/QrScanModal.tsx
+++ web/src/components/QrScanModal/QrScanModal.tsx
@@ -6,26 +6,19 @@ import { navigate, routes } from '@redwoodjs/router'
 interface Props extends ModalProps {}
 
 const QrScanModal = ({ opened, onClose }: Props) => {
-  const [isLegacyId, setIsLegacyId] = React.useState(false)
   return (
     <Modal opened={opened} onClose={onClose} title="Scan QR Code">
       <QrReader
         onResult={(result, error) => {
           if (result) {
-            const text = result.getText()
             navigate(routes.item({ id: result.getText() }))
           }
 
           if (error) {
-            console.info('Unable to read QR code:', error)
+            console.info('Unable to read QR code:', error.message)
           }
         }}
         constraints={{ facingMode: ['environment', 'user'] }}
-      />
-      <Checkbox
-        label="Legacy ID"
-        checked={isLegacyId}
-        onChange={(event) => setIsLegacyId(event.currentTarget.checked)}
       />
     </Modal>
   )
